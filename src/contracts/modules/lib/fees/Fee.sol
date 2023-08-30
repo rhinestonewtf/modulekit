3d579d2070a9d32cad6d0fd960bbbb3739a4f873
zeroknots <zeroknots@protonmail.com>
2023-08-30 12:44:31 +0700

    fixing fee wrapper

--- src/contracts/modules/lib/fees/Fee.sol
+++ src/contracts/modules/lib/fees/Fee.sol
@@ -7,5 +7,6 @@ import "forge-std/interfaces/IERC20.sol";
 
 function _payFee(address vault, address token, address amount) view returns (ExecutorAction memory action) {
     action.to = payable(token);
-    action.data = abi.encodeCall(IERC20.transfer, vault, amount);
+    action.data = abi.encodeWithSelector(IERC20.transfer.selector, vault, amount);
 }
+
