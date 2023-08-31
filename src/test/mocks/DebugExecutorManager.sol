99be0d1fdf7fc3bd5b3ec7b04f639057ad243eb0
zeroknots <zeroknots@protonmail.com>
2023-08-31 13:06:41 +0700

    feat: adding debug contract to allow unit tests for executors

---
+++ src/test/mocks/DebugExecutorManager.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "../../contracts/modules/executors/IExecutorBase.sol";
+
+struct DebugData {
+    address account;
+    ExecutorTransaction transaction;
+}
+
+contract DebugManagerExecutor is IExecutorManager {
+    DebugData public storeData;
+
+    ExecutorTransaction public transaction;
+
+    function executeTransaction(address account, ExecutorTransaction calldata transaction)
+        external
+        override
+        returns (bytes[] memory data)
+    {
+        storeData.account = account;
+        storeData.transaction = transaction;
+    }
+}
