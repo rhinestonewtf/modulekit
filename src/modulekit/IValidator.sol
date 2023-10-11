2aad4330a7a68b9a4989fc5e817cae70450b9b20
zeroknots <zeroknots@protonmail.com>
2023-10-11 11:16:43 +0700

    write validator base

--- src/modulekit/IValidator.sol
+++ src/modulekit/IValidator.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.19;
 
 import "../common/erc4337/UserOperation.sol";
+import "../common/IERC1271.sol";
 
 uint256 constant VALIDATION_SUCCESS = 0;
+uint256 constant VALIDATION_FAILED = 1;
 
-interface IValidatorModule {
+interface IValidator is IERC1271 {
     function validateUserOp(
         UserOperation calldata userOp,
         bytes32 userOpHash
--- src/modulekit/ValidatorBase.sol
+++ src/modulekit/ValidatorBase.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
 
+import "./IValidator.sol";
+
+contract ValidatorBase is IValidator {
+    function isValidSignature(
+        bytes32 hash,
+        bytes memory signature
+    )
+        external
+        view
+        virtual
+        override
+        returns (bytes4 magicValue)
+    {
+        return 0xffffffff;
+    }
+
+    function validateUserOp(
+        UserOperation calldata userOp,
+        bytes32 userOpHash
+    )
+        external
+        virtual
+        override
+        returns (uint256)
+    {
+        return VALIDATION_FAILED;
+    }
+}
