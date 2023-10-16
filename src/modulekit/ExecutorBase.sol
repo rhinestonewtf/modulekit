7a40cdc8265331ba51d5f97ae5ca6233e1049b49
kopy-kat <kopp.konrad@gmail.com>
2023-10-16 17:38:39 +0100

    feat: convert to base modules to abstract contracts and add default supportsInterface functionality

--- src/modulekit/ExecutorBase.sol
+++ src/modulekit/ExecutorBase.sol
@@ -3,28 +3,23 @@ pragma solidity ^0.8.21;
 
 import "./IExecutor.sol";
 
-contract ExecutorBase is IExecutorBase {
-    function name() external view virtual returns (string memory name) {
-        name = "ExecutorBase";
-    }
+abstract contract ExecutorBase is IExecutorBase {
+    function name() external view virtual returns (string memory name);
 
-    function version() external view virtual returns (string memory version) {
-        version = "0.0.1";
-    }
+    function version() external view virtual returns (string memory version);
 
     function metadataProvider()
         external
         view
         virtual
-        returns (uint256 providerType, bytes memory location)
-    {
-        providerType = 0;
-        location = "";
-    }
+        returns (uint256 providerType, bytes memory location);
 
-    function requiresRootAccess() external view virtual returns (bool requiresRootAccess) {
-        requiresRootAccess = false;
-    }
+    function requiresRootAccess() external view virtual returns (bool requiresRootAccess);
 
-    function supportsInterface(bytes4 interfaceID) external view virtual override returns (bool) { }
+    function supportsInterface(bytes4 interfaceID) external view virtual override returns (bool) {
+        return interfaceID == IExecutorBase.name.selector
+            || interfaceID == IExecutorBase.version.selector
+            || interfaceID == IExecutorBase.metadataProvider.selector
+            || interfaceID == IExecutorBase.requiresRootAccess.selector;
+    }
 }
--- src/modulekit/ValidatorBase.sol
+++ src/modulekit/ValidatorBase.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.21;
 
 import "./IValidator.sol";
 
-contract ValidatorBase is IValidator {
+abstract contract ValidatorBase is IValidator {
     function isValidSignature(
         bytes32 hash,
         bytes memory signature
@@ -12,10 +12,7 @@ contract ValidatorBase is IValidator {
         view
         virtual
         override
-        returns (bytes4 magicValue)
-    {
-        return 0xffffffff;
-    }
+        returns (bytes4 magicValue);
 
     function validateUserOp(
         UserOperation calldata userOp,
@@ -24,10 +21,10 @@ contract ValidatorBase is IValidator {
         external
         virtual
         override
-        returns (uint256)
-    {
-        return VALIDATION_FAILED;
-    }
+        returns (uint256);
 
-    function supportsInterface(bytes4 interfaceID) external view virtual override returns (bool) { }
+    function supportsInterface(bytes4 interfaceID) external view virtual override returns (bool) {
+        return interfaceID == IERC1271.isValidSignature.selector
+            || interfaceID == IValidator.validateUserOp.selector;
+    }
 }
