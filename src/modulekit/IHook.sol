a2d76028216dbb522f75e3a74b7748b2e46566f4
kopy-kat <kopp.konrad@gmail.com>
2023-10-19 13:48:53 +0100

    feat: add hook base

--- src/modulekit/IHook.sol
+++ src/modulekit/IHook.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import "./IExecutor.sol";
+import { ExecutorTransaction } from "./IExecutor.sol";
 
 interface IHook {
     function preCheck(
