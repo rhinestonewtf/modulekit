56ceca717c3c62642ceb1e9c2e940724ef54efbf
zeroknots <102132718+zeroknots@users.noreply.github.com>
2023-10-23 08:35:13 +0700

    feat: ERC1271 based signature condition (#26)

---
+++ src/modulekit/conditions/SignatureCondition.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { ICondition } from "../IExecutor.sol";
+import { IERC1271, ERC1271_MAGICVALUE } from "../../common/IERC1271.sol";
+
+contract SignatureCondtion is ICondition {
+    struct Params {
+        bytes32 hash;
+        bytes signature;
+    }
+
+    event ScheduleUpdated(address account, uint256 lastExecuted);
+
+    function checkCondition(
+        address account,
+        address,
+        bytes calldata conditionData,
+        bytes calldata
+    )
+        external
+        view
+        override
+        returns (bool valid)
+    {
+        Params memory params = abi.decode(conditionData, (Params));
+        if (IERC1271(account).isValidSignature(params.hash, params.signature) == ERC1271_MAGICVALUE)
+        {
+            return true;
+        }
+    }
+}
