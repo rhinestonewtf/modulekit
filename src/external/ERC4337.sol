35e2f53c8352aef050b77638e4ea87ba82b275c3
zeroknots <zeroknots@protonmail.com>
2024-01-03 21:44:25 +0700

    fixed pnpm

--- src/external/ERC4337.sol
+++ src/external/ERC4337.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import { UserOperation, UserOperationLib } from "account-abstraction/interfaces/UserOperation.sol";
+import { UserOperation } from "account-abstraction/interfaces/UserOperation.sol";
+import { UserOperationLib } from "account-abstraction/core/UserOperationLib.sol";
 import { IEntryPoint } from "account-abstraction/interfaces/IEntryPoint.sol";
 import { ValidationData, _packValidationData } from "account-abstraction/core/Helpers.sol";
