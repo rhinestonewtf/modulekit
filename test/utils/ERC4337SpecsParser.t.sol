165ab1c4548b838e1b52093edabbddb127aeb9dc
kopy-kat <kopp.konrad@gmail.com>
2024-02-03 16:14:47 +0000

    feat: add tests for storage restrictions

--- test/utils/ERC4337SpecsParser.t.sol
+++ test/utils/ERC4337SpecsParser.t.sol
@@ -7,6 +7,7 @@ import "src/ModuleKit.sol";
 import "src/test/RhinestoneModuleKit.sol";
 import "forge-std/console2.sol";
 import { writeSimulateUserOp } from "src/test/utils/Log.sol";
+import { ERC4337SpecsParser } from "src/test/utils/ERC4337Helpers.sol";
 /* solhint-enable no-global-import */
 
 contract SpecsTestValidator {
@@ -15,20 +16,42 @@ contract SpecsTestValidator {
         uint256 data2;
     }
 
-    mapping(address => uint256) data;
+    uint256 data;
+    mapping(address => uint256) singleData;
     mapping(uint256 => mapping(address => uint256)) nestedData;
+    mapping(address => mapping(uint256 => uint256)) nestedDataReverse;
     mapping(uint256 => mapping(address => DataStruct)) nestedDataStruct;
 
+    function setData(uint256 value) public {
+        data = value;
+    }
+
     function setData(address addr, uint256 value) public {
-        data[addr] = value;
+        singleData[addr] = value;
     }
 
     function setNestedData(address addr, uint256 value) public {
         nestedData[value][addr] = value;
     }
 
+    function setNestedDataReverse(address addr, uint256 value) public {
+        nestedDataReverse[addr][value] = value;
+    }
+
     function setNestedDataStruct(address addr, uint256 value) public {
         nestedDataStruct[value][addr] = DataStruct({ data1: value, data2: value });
+    }
+
+    function setNestedDataWithOffset(address addr, uint256 value, uint256 offset) public {
+        bytes32 slot;
+        assembly {
+            slot := singleData.slot
+        }
+        bytes32 _slot = keccak256(abi.encode(addr, slot));
+        bytes32 offsetSlot = bytes32(uint256(_slot) + offset);
+        assembly {
+            sstore(offsetSlot, value)
+        }
     }
 
     function onInstall(bytes memory) external {
@@ -49,6 +72,18 @@ contract SpecsTestValidator {
             setNestedData(msg.sender, 2);
         } else if (mode == 3) {
             setNestedDataStruct(msg.sender, 3);
+        } else if (mode == 4) {
+            setData(address(1), 4);
+        } else if (mode == 5) {
+            setNestedData(address(1), 5);
+        } else if (mode == 6) {
+            setData(6);
+        } else if (mode == 7) {
+            setNestedDataReverse(address(1), 7);
+        } else if (mode == 8) {
+            setNestedDataWithOffset(msg.sender, 8, 128);
+        } else if (mode == 9) {
+            setNestedDataWithOffset(msg.sender, 9, 129);
         }
         return 0;
     }
@@ -72,11 +107,12 @@ contract ERC4337SpecsParserTest is Test, RhinestoneModuleKit {
         ERC7579BootstrapConfig memory hook = _emptyConfig();
         ERC7579BootstrapConfig memory fallBack = _emptyConfig();
 
-        console2.log(validators[0].module);
-
         // Setup account
         instance = makeRhinestoneAccount("account1", validators, executors, hook, fallBack);
         vm.deal(instance.account, 1000 ether);
+
+        // Simulate userOperation
+        writeSimulateUserOp(true);
     }
 
     function exec(address receiver, bytes memory callData) internal {
@@ -90,9 +126,6 @@ contract ERC4337SpecsParserTest is Test, RhinestoneModuleKit {
         uint256 value = 10 gwei;
         bytes memory callData = "";
         bytes memory signature = "";
-
-        // Simulate userOperation
-        writeSimulateUserOp(true);
 
         // Create userOperation
         UserOpData memory userOpData = instance.getExecOps({
@@ -114,9 +147,6 @@ contract ERC4337SpecsParserTest is Test, RhinestoneModuleKit {
         bytes memory callData = "";
         bytes memory signature = "";
 
-        // Simulate userOperation
-        writeSimulateUserOp(true);
-
         // Create userOperation
         UserOpData memory userOpData = instance.getExecOps({
             target: receiver,
@@ -137,9 +167,6 @@ contract ERC4337SpecsParserTest is Test, RhinestoneModuleKit {
         bytes memory callData = "";
         bytes memory signature = "";
 
-        // Simulate userOperation
-        writeSimulateUserOp(true);
-
         // Create userOperation
         UserOpData memory userOpData = instance.getExecOps({
             target: receiver,
@@ -151,5 +178,165 @@ contract ERC4337SpecsParserTest is Test, RhinestoneModuleKit {
         userOpData.userOp.signature = abi.encodePacked(bytes32(uint256(3)));
 
         userOpData.execUserOps();
+    }
+
+    function singleMapping__RevertWhen__InvalidKey() public {
+        // Create userOperation fields
+        address receiver = makeAddr("receiver");
+        uint256 value = 10 gwei;
+        bytes memory callData = "";
+        bytes memory signature = "";
+
+        // Create userOperation
+        UserOpData memory userOpData = instance.getExecOps({
+            target: receiver,
+            value: value,
+            callData: callData,
+            txValidator: address(validator)
+        });
+
+        userOpData.userOp.signature = abi.encodePacked(bytes32(uint256(4)));
+
+        userOpData.execUserOps();
+    }
+
+    function testSingleMapping__RevertWhen__InvalidKey() public {
+        vm.expectRevert(ERC4337SpecsParser.InvalidStorageLocation.selector);
+        (bool success,) = address(this).call(
+            abi.encodeWithSelector(this.singleMapping__RevertWhen__InvalidKey.selector)
+        );
+        assertFalse(success);
+    }
+
+    function nestedMapping__RevertWhen__InvalidKey() public {
+        // Create userOperation fields
+        address receiver = makeAddr("receiver");
+        uint256 value = 10 gwei;
+        bytes memory callData = "";
+        bytes memory signature = "";
+
+        // Create userOperation
+        UserOpData memory userOpData = instance.getExecOps({
+            target: receiver,
+            value: value,
+            callData: callData,
+            txValidator: address(validator)
+        });
+
+        userOpData.userOp.signature = abi.encodePacked(bytes32(uint256(5)));
+
+        userOpData.execUserOps();
+    }
+
+    function testNestedMapping__RevertWhen__InvalidKey() public {
+        vm.expectRevert(ERC4337SpecsParser.InvalidStorageLocation.selector);
+        (bool success,) = address(this).call(
+            abi.encodeWithSelector(this.nestedMapping__RevertWhen__InvalidKey.selector)
+        );
+        assertFalse(success);
+    }
+
+    function simpleStorage__RevertWhen__InvalidSlot() public {
+        // Create userOperation fields
+        address receiver = makeAddr("receiver");
+        uint256 value = 10 gwei;
+        bytes memory callData = "";
+        bytes memory signature = "";
+
+        // Create userOperation
+        UserOpData memory userOpData = instance.getExecOps({
+            target: receiver,
+            value: value,
+            callData: callData,
+            txValidator: address(validator)
+        });
+
+        userOpData.userOp.signature = abi.encodePacked(bytes32(uint256(6)));
+
+        userOpData.execUserOps();
+    }
+
+    function testSimpleStorage__RevertWhen__InvalidSlot() public {
+        vm.expectRevert(ERC4337SpecsParser.InvalidStorageLocation.selector);
+        (bool success,) = address(this).call(
+            abi.encodeWithSelector(this.simpleStorage__RevertWhen__InvalidSlot.selector)
+        );
+        assertFalse(success);
+    }
+
+    function nestedMapping__RevertWhen__InvalidArgOrder() public {
+        // Create userOperation fields
+        address receiver = makeAddr("receiver");
+        uint256 value = 10 gwei;
+        bytes memory callData = "";
+        bytes memory signature = "";
+
+        // Create userOperation
+        UserOpData memory userOpData = instance.getExecOps({
+            target: receiver,
+            value: value,
+            callData: callData,
+            txValidator: address(validator)
+        });
+
+        userOpData.userOp.signature = abi.encodePacked(bytes32(uint256(7)));
+
+        userOpData.execUserOps();
+    }
+
+    function testNestedMapping__RevertWhen__InvalidArgOrder() public {
+        vm.expectRevert(ERC4337SpecsParser.InvalidStorageLocation.selector);
+        (bool success,) = address(this).call(
+            abi.encodeWithSelector(this.nestedMapping__RevertWhen__InvalidArgOrder.selector)
+        );
+        assertFalse(success);
+    }
+
+    function testStructMapping__With__Offset() public {
+        // Create userOperation fields
+        address receiver = makeAddr("receiver");
+        uint256 value = 10 gwei;
+        bytes memory callData = "";
+        bytes memory signature = "";
+
+        // Create userOperation
+        UserOpData memory userOpData = instance.getExecOps({
+            target: receiver,
+            value: value,
+            callData: callData,
+            txValidator: address(validator)
+        });
+
+        userOpData.userOp.signature = abi.encodePacked(bytes32(uint256(8)));
+
+        userOpData.execUserOps();
+    }
+
+    function structMapping__RevertWhen__OutOfBounds() public {
+        // Create userOperation fields
+        address receiver = makeAddr("receiver");
+        uint256 value = 10 gwei;
+        bytes memory callData = "";
+        bytes memory signature = "";
+
+        // Create userOperation
+        UserOpData memory userOpData = instance.getExecOps({
+            target: receiver,
+            value: value,
+            callData: callData,
+            txValidator: address(validator)
+        });
+
+        userOpData.userOp.signature = abi.encodePacked(bytes32(uint256(9)));
+
+        userOpData.execUserOps();
+    }
+
+    function testStructMapping__RevertWhen__OutOfBounds() public {
+        // vm.expectRevert(ERC4337SpecsParser.InvalidStorageLocation.selector);
+        (bool success,) = address(this).call(
+            abi.encodeWithSelector(this.structMapping__RevertWhen__OutOfBounds.selector)
+        );
+        assertFalse(success);
     }
 }
