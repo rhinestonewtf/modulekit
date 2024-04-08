bcb0d67685fb6c2ebaa7f247b13c0175f0ed6991
kopy-kat <kopp.konrad@gmail.com>
2024-04-08 12:49:14 +0200

    fix: scheduled transfers module

--- examples/src/ScheduledTransactions/ScheduledTransfers.sol
+++ examples/src/ScheduledTransactions/ScheduledTransfers.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import { IERC7579Account, Execution } from "modulekit/src/Accounts.sol";
+import { IERC7579Account } from "modulekit/src/Accounts.sol";
 import { SchedulingBase } from "./SchedulingBase.sol";
 import { ModeLib } from "erc7579/lib/ModeLib.sol";
 import { ExecutionLib } from "erc7579/lib/ExecutionLib.sol";
+import { IERC20 } from "forge-std/interfaces/IERC20.sol";
 
 contract ScheduledTransfers is SchedulingBase {
     /*//////////////////////////////////////////////////////////////////////////
@@ -14,15 +15,24 @@ contract ScheduledTransfers is SchedulingBase {
     function executeOrder(uint256 jobId) external override canExecute(jobId) {
         ExecutionConfig storage executionConfig = _executionLog[msg.sender][jobId];
 
-        Execution memory execution = abi.decode(executionConfig.executionData, (Execution));
-
         executionConfig.lastExecutionTime = uint48(block.timestamp);
         executionConfig.numberOfExecutionsCompleted += 1;
 
-        IERC7579Account(msg.sender).executeFromExecutor(
-            ModeLib.encodeSimpleSingle(),
-            ExecutionLib.encodeSingle(execution.target, execution.value, execution.callData)
-        );
+        (address recipient, address token, uint256 amount) =
+            abi.decode(executionConfig.executionData, (address, address, uint256));
+
+        if (token == address(0)) {
+            IERC7579Account(msg.sender).executeFromExecutor(
+                ModeLib.encodeSimpleSingle(), ExecutionLib.encodeSingle(recipient, amount, "")
+            );
+        } else {
+            IERC7579Account(msg.sender).executeFromExecutor(
+                ModeLib.encodeSimpleSingle(),
+                ExecutionLib.encodeSingle(
+                    token, 0, abi.encodeWithSelector(IERC20.transfer.selector, recipient, amount)
+                )
+            );
+        }
 
         emit ExecutionTriggered(msg.sender, jobId);
     }
