6e246247a370181ef597c211cde54f8a0ad6b40c
kopy-kat <kopp.konrad@gmail.com>
2024-04-10 14:03:18 +0200

    feat: hookbase and multiplexer prototype

--- examples/src/HookMultiPlexer/HookMultiPlexer.sol
+++ examples/src/HookMultiPlexer/HookMultiPlexer.sol
@@ -1,37 +1,79 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import { ERC7579HookBase } from "modulekit/src/Modules.sol";
+import { ERC7579HookBase, ERC7579ExecutorBase } from "modulekit/src/Modules.sol";
+import { ERC7579HookBaseNew } from "modulekit/src/modules/ERC7579HookBaseNew.sol";
+import { SENTINEL, SentinelListLib } from "sentinellist/SentinelList.sol";
+import { IERC7579Account, Execution } from "modulekit/src/Accounts.sol";
+import { ModeLib } from "erc7579/lib/ModeLib.sol";
+import { ExecutionLib } from "erc7579/lib/ExecutionLib.sol";
 
-contract HookMultiPlexer is ERC7579HookBase {
+contract HookMultiPlexer is ERC7579HookBase, ERC7579ExecutorBase {
+    using SentinelListLib for SentinelListLib.SentinelList;
+
     /*//////////////////////////////////////////////////////////////////////////
                             CONSTANTS & STORAGE
     //////////////////////////////////////////////////////////////////////////*/
 
     error NoHookRegistered(address smartAccount);
     error HookReverted(address hookAddress, bytes hookData);
+    error InvalidHookInitDataLength();
 
-    event HookAdded(address indexed smartAccount, address hookAddress);
-
-    mapping(address account => address) hook;
+    mapping(address account => SentinelListLib.SentinelList) hooks;
 
     /*//////////////////////////////////////////////////////////////////////////
                                      CONFIG
     //////////////////////////////////////////////////////////////////////////*/
 
     function onInstall(bytes calldata data) external {
-        address hookAddress = abi.decode(data, (address));
-        hook[msg.sender] = hookAddress;
+        address account = msg.sender;
+        if (isInitialized(account)) {
+            revert AlreadyInitialized(account);
+        }
 
-        emit HookAdded(msg.sender, hookAddress);
+        (address[] memory hookAddresses, bytes[] memory initDatas) =
+            abi.decode(data, (address[], bytes[]));
+
+        if (hookAddresses.length != initDatas.length) {
+            revert InvalidHookInitDataLength();
+        }
+
+        hooks[account].init();
+
+        uint256 hooksLength = hookAddresses.length * 2;
+
+        Execution[] memory executions = new Execution[](hooksLength);
+
+        for (uint256 i = 0; i < hooksLength; i += 2) {
+            address _hook = hookAddresses[i];
+            // TODO: add registry check?
+            hooks[account].push(_hook);
+            executions[i] = Execution({
+                target: _hook,
+                value: 0,
+                callData: abi.encodeWithSelector(this.onInstall.selector, initDatas[i])
+            });
+
+            executions[i + 1] = Execution({
+                target: _hook,
+                value: 0,
+                callData: abi.encodeWithSelector(
+                    ERC7579HookBaseNew.setTrustedForwarder.selector, address(this)
+                )
+            });
+        }
+
+        IERC7579Account(account).executeFromExecutor(
+            ModeLib.encodeSimpleBatch(), ExecutionLib.encodeBatch(executions)
+        );
     }
 
     function onUninstall(bytes calldata) external override {
-        delete hook[msg.sender];
+        // TODO: Implement onUninstall
     }
 
-    function isInitialized(address smartAccount) external view returns (bool) {
-        return hook[smartAccount] != address(0);
+    function isInitialized(address smartAccount) public view returns (bool) {
+        return hooks[smartAccount].alreadyInitialized();
     }
 
     /*//////////////////////////////////////////////////////////////////////////
@@ -46,24 +88,34 @@ contract HookMultiPlexer is ERC7579HookBase {
         external
         returns (bytes memory hookReturnData)
     {
-        address hookAddress = hook[msg.sender];
-        if (hookAddress == address(0)) {
-            revert NoHookRegistered(msg.sender);
+        (address[] memory selectedHooks,) = hooks[msg.sender].getEntriesPaginated(SENTINEL, 10);
+        uint256 hooksLength = selectedHooks.length;
+
+        bytes[] memory hookReturnDataArray = new bytes[](hooksLength);
+
+        for (uint256 i = 0; i < hooksLength; i++) {
+            address hookAddress = selectedHooks[i];
+
+            if (hookAddress == address(0)) {
+                revert NoHookRegistered(msg.sender);
+            }
+
+            (bool success, bytes memory _hookReturnData) = hookAddress.call(
+                abi.encodePacked(
+                    abi.encodeWithSelector(this.preCheck.selector, msgSender, msgValue, msgData),
+                    address(this),
+                    msg.sender
+                )
+            );
+
+            if (!success) {
+                revert HookReverted(hookAddress, _hookReturnData);
+            }
+
+            hookReturnDataArray[i] = _hookReturnData;
         }
 
-        bool success;
-
-        (success, hookReturnData) = hookAddress.call(
-            abi.encodePacked(
-                abi.encodeWithSelector(this.preCheck.selector, msgSender, msgValue, msgData),
-                address(this),
-                msg.sender
-            )
-        );
-
-        if (!success) {
-            revert HookReverted(hookAddress, hookReturnData);
-        }
+        hookReturnData = abi.encode(hookReturnDataArray);
     }
 
     function postCheck(
@@ -73,23 +125,28 @@ contract HookMultiPlexer is ERC7579HookBase {
     )
         external
     {
-        address hookAddress = hook[msg.sender];
-        if (hookAddress == address(0)) {
-            revert NoHookRegistered(msg.sender);
-        }
+        (address[] memory selectedHooks,) = hooks[msg.sender].getEntriesPaginated(SENTINEL, 10);
+        uint256 hooksLength = selectedHooks.length;
 
-        (bool success, bytes memory hookReturnData) = hookAddress.call(
-            abi.encodePacked(
-                abi.encodeWithSelector(
-                    this.postCheck.selector, hookData, executionSuccess, executionReturnValue
-                ),
-                address(this),
-                msg.sender
-            )
-        );
+        for (uint256 i = 0; i < hooksLength; i++) {
+            address hookAddress = selectedHooks[i];
+            if (hookAddress == address(0)) {
+                revert NoHookRegistered(msg.sender);
+            }
 
-        if (!success) {
-            revert HookReverted(hookAddress, hookReturnData);
+            (bool success, bytes memory hookReturnData) = hookAddress.call(
+                abi.encodePacked(
+                    abi.encodeWithSelector(
+                        this.postCheck.selector, hookData, executionSuccess, executionReturnValue
+                    ),
+                    address(this),
+                    msg.sender
+                )
+            );
+
+            if (!success) {
+                revert HookReverted(hookAddress, hookReturnData);
+            }
         }
     }
 
@@ -102,10 +159,10 @@ contract HookMultiPlexer is ERC7579HookBase {
     }
 
     function version() external pure returns (string memory) {
-        return "0.0.1";
+        return "1.0.0";
     }
 
     function isModuleType(uint256 typeID) external pure override returns (bool) {
-        return typeID == TYPE_HOOK;
+        return typeID == TYPE_HOOK || typeID == TYPE_EXECUTOR;
     }
 }
--- examples/test/mocks/MockHook.sol
+++ examples/test/mocks/MockHook.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import { ERC7579HookBase } from "modulekit/src/Modules.sol";
+import { ERC7579HookBaseNew } from "modulekit/src/modules/ERC7579HookBaseNew.sol";
 import {
     ModeLib,
     CallType,
@@ -14,61 +14,10 @@ import {
 
 import "forge-std/console2.sol";
 
-contract MockHook is ERC7579HookBase {
+contract MockHook is ERC7579HookBaseNew {
     function onInstall(bytes calldata data) external override { }
 
     function onUninstall(bytes calldata data) external override { }
-
-    // Handled by the base
-    // TODO: best devx for setting forwarder
-    mapping(address account => address) public trustedForwarder;
-
-    function setForwarder(address forwarder) external {
-        trustedForwarder[msg.sender] = forwarder;
-    }
-
-    function isTrustedForwarder(address forwarder, address account) public view returns (bool) {
-        return true;
-        return forwarder == trustedForwarder[account];
-    }
-
-    function _msgSender() internal view returns (address account) {
-        account = msg.sender;
-        address _account;
-        address forwarder;
-        if (msg.data.length >= 40) {
-            assembly {
-                _account := shr(96, calldataload(sub(calldatasize(), 20)))
-                forwarder := shr(96, calldataload(sub(calldatasize(), 40)))
-            }
-            if (forwarder == msg.sender && isTrustedForwarder(forwarder, _account)) {
-                account = _account;
-            }
-        }
-    }
-
-    function preCheck(
-        address msgSender,
-        uint256 msgValue,
-        bytes calldata msgData
-    )
-        external
-        returns (bytes memory hookData)
-    {
-        return _preCheck(_msgSender(), msgSender, msgValue, msgData);
-    }
-
-    function postCheck(
-        bytes calldata hookData,
-        bool executionSuccess,
-        bytes calldata executionReturnValue
-    )
-        external
-    {
-        _postCheck(_msgSender(), hookData, executionSuccess, executionReturnValue);
-    }
-
-    // End of handled by the base
 
     function _preCheck(
         address account,
@@ -77,6 +26,7 @@ contract MockHook is ERC7579HookBase {
         bytes calldata msgData
     )
         internal
+        override
         returns (bytes memory hookData)
     {
         ModeSelector mode = ModeSelector.wrap(bytes4(msgData[10:14]));
@@ -97,6 +47,7 @@ contract MockHook is ERC7579HookBase {
         bytes calldata executionReturnValue
     )
         internal
+        override
     {
         if (keccak256(hookData) == keccak256(abi.encode("revertPost"))) {
             revert("revertPost");
