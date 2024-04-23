533717dcf329bb07c09d0b4c7e50846ae9406e66
kopy-kat <kopp.konrad@gmail.com>
2024-04-23 15:19:05 +0100

    feat: iterate on mfa

--- examples/src/MultiFactor/MultiFactor.sol
+++ examples/src/MultiFactor/MultiFactor.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.25;
 import { ERC7579ValidatorBase } from "modulekit/src/Modules.sol";
 import { PackedUserOperation } from "modulekit/src/external/ERC4337.sol";
 import { IStatelessValidator } from "modulekit/src/interfaces/IStatelessValidator.sol";
+import { IERC7484 } from "modulekit/src/interfaces/IERC7484.sol";
+import { MODULE_TYPE_VALIDATOR } from "modulekit/src/external/ERC7579.sol";
 
 contract MultiFactor is ERC7579ValidatorBase {
     /*//////////////////////////////////////////////////////////////////////////
@@ -11,17 +13,15 @@ contract MultiFactor is ERC7579ValidatorBase {
     //////////////////////////////////////////////////////////////////////////*/
 
     error InvalidThreshold(uint256 length, uint256 threshold);
-    error ValidatorIsAlreadyUsed(address smartaccount, address validator);
-    error InvalidParams();
     error InvalidParamsLength();
 
     event ValidatorAdded(
-        address indexed smartAccount, address indexed validator, uint256 id, uint8 iteration
+        address indexed smartAccount, address indexed validator, uint256 id, uint256 iteration
     );
     event ValidatorRemoved(
-        address indexed smartAccount, address indexed validator, uint256 id, uint8 iteration
+        address indexed smartAccount, address indexed validator, uint256 id, uint256 iteration
     );
-    event IterationIncreased(address indexed smartAccount, uint8 iteration);
+    event IterationIncreased(address indexed smartAccount, uint256 iteration);
 
     struct Config {
         uint32 iteration;
@@ -29,26 +29,29 @@ contract MultiFactor is ERC7579ValidatorBase {
     }
 
     struct Validator {
-        address validatorAddress;
-        uint96 id;
-        bytes validatorData;
+        bytes32 validatorAndId; // abi.encodePacked(uint92(id), address(validator))
+        bytes data;
     }
 
-    struct ValidatorToUse {
-        address validatorAddress;
-        uint96 id;
-        bytes signature;
-    }
-
-    // account => threshold
+    // account => Config
     mapping(address account => Config) public config;
-    // validator => account => validatorData
+    // iteration => validatorAddress => id => account => data
     mapping(
-        uint32 iteration
+        uint256 iteration
             => mapping(
-                address validatorAddress => mapping(uint96 id => mapping(address account => bytes))
+                address validatorAddress => mapping(uint256 id => mapping(address account => bytes))
             )
     ) public validatorData;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                    CONSTRUCTOR
+    //////////////////////////////////////////////////////////////////////////*/
+
+    IERC7484 public immutable registry;
+
+    constructor(IERC7484 _registry) {
+        registry = _registry;
+    }
 
     /*//////////////////////////////////////////////////////////////////////////
                                      CONFIG
@@ -60,32 +63,70 @@ contract MultiFactor is ERC7579ValidatorBase {
 
         (Validator[] memory validators, uint8 threshold) = abi.decode(data, (Validator[], uint8));
 
-        uint32 iteration = config[account].iteration;
+        uint256 iteration = config[account].iteration;
         uint256 length = validators.length;
 
         if (length < threshold) revert InvalidThreshold(length, threshold);
 
         for (uint256 i; i < length; i++) {
-            // TODO: check registry
-
             Validator memory validator = validators[i];
-            validatorData[iteration][validator.validatorAddress][validator.id][account] =
-                validator.validatorData;
+            (address validatorAddress, uint96 id) = _decodeValidatorAndId(validator.validatorAndId);
+
+            registry.checkForAccount({
+                smartAccount: msg.sender,
+                module: validatorAddress,
+                moduleType: MODULE_TYPE_VALIDATOR
+            });
+
+            validatorData[iteration][validatorAddress][id][account] = validator.data;
+
+            emit ValidatorAdded(account, validatorAddress, id, iteration);
         }
 
         config[account].threshold = threshold;
     }
 
     function onUninstall(bytes calldata) external {
-        // TODO
         address account = msg.sender;
 
         config[account].threshold = 0;
         config[account].iteration++;
+
+        emit IterationIncreased(account, config[account].iteration);
     }
 
     function isInitialized(address account) public view returns (bool) {
         return config[account].threshold != 0;
+    }
+
+    function addValidator(
+        address _validatorAddress,
+        uint96 _id,
+        bytes calldata _validatorData
+    )
+        external
+    {
+        address account = msg.sender;
+        uint256 iteration = config[account].iteration;
+
+        registry.checkForAccount({
+            smartAccount: msg.sender,
+            module: _validatorAddress,
+            moduleType: MODULE_TYPE_VALIDATOR
+        });
+
+        validatorData[iteration][_validatorAddress][_id][account] = _validatorData;
+
+        emit ValidatorAdded(account, _validatorAddress, _id, iteration);
+    }
+
+    function removeValidator(address _validatorAddress, uint96 _id) external {
+        address account = msg.sender;
+        uint256 iteration = config[account].iteration;
+
+        delete validatorData[iteration][_validatorAddress][_id][account];
+
+        emit ValidatorRemoved(account, _validatorAddress, _id, iteration);
     }
 
     /*//////////////////////////////////////////////////////////////////////////
@@ -101,9 +142,9 @@ contract MultiFactor is ERC7579ValidatorBase {
         override
         returns (ValidationData)
     {
-        (ValidatorToUse[] memory validatorsToUse) = abi.decode(userOp.signature, (ValidatorToUse[]));
+        (Validator[] memory validators) = abi.decode(userOp.signature, (Validator[]));
 
-        bool isValid = _validateSignatureWithConfig(validatorsToUse, userOpHash);
+        bool isValid = _validateSignatureWithConfig(validators, userOpHash);
 
         if (isValid) {
             return VALIDATION_SUCCESS;
@@ -112,7 +153,7 @@ contract MultiFactor is ERC7579ValidatorBase {
     }
 
     function isValidSignatureWithSender(
-        address sender,
+        address,
         bytes32 hash,
         bytes calldata data
     )
@@ -122,9 +163,9 @@ contract MultiFactor is ERC7579ValidatorBase {
         override
         returns (bytes4)
     {
-        (ValidatorToUse[] memory validatorsToUse) = abi.decode(data, (ValidatorToUse[]));
+        (Validator[] memory validators) = abi.decode(data, (Validator[]));
 
-        bool isValid = _validateSignatureWithConfig(validatorsToUse, hash);
+        bool isValid = _validateSignatureWithConfig(validators, hash);
 
         if (isValid) {
             return EIP1271_SUCCESS;
@@ -137,29 +178,31 @@ contract MultiFactor is ERC7579ValidatorBase {
     //////////////////////////////////////////////////////////////////////////*/
 
     function _validateSignatureWithConfig(
-        ValidatorToUse[] memory validatorsToUse,
+        Validator[] memory validators,
         bytes32 hash
     )
         internal
         view
         returns (bool)
     {
-        uint256 validatorsLength = validatorsToUse.length;
+        uint256 validatorsLength = validators.length;
         if (validatorsLength == 0) revert InvalidParamsLength();
 
         address account = msg.sender;
-        uint32 iteration = config[account].iteration;
+        uint256 iteration = config[account].iteration;
 
         uint256 validCount;
 
         for (uint256 i; i < validatorsLength; i++) {
-            ValidatorToUse memory validatorToUse = validatorsToUse[i];
+            Validator memory validator = validators[i];
 
-            bytes memory _validatorData = validatorData[iteration][validatorToUse.validatorAddress][validatorToUse
-                .id][account];
+            (address validatorAddress, uint96 id) = _decodeValidatorAndId(validator.validatorAndId);
 
-            bool isValid = IStatelessValidator(validatorToUse.validatorAddress)
-                .validateSignatureWithData(hash, validatorToUse.signature, _validatorData);
+            bytes memory _validatorData = validatorData[iteration][validatorAddress][id][account];
+
+            bool isValid = IStatelessValidator(validatorAddress).validateSignatureWithData(
+                hash, validator.data, _validatorData
+            );
 
             if (isValid) {
                 validCount++;
@@ -170,6 +213,15 @@ contract MultiFactor is ERC7579ValidatorBase {
             return true;
         }
         return false;
+    }
+
+    function _decodeValidatorAndId(bytes32 validatorAndId)
+        internal
+        pure
+        returns (address, uint96)
+    {
+        // validatorAndId = abi.encodePacked(uint96(id), address(validator))
+        return (address(bytes20(validatorAndId)), uint96(uint256(validatorAndId)));
     }
 
     /*//////////////////////////////////////////////////////////////////////////
