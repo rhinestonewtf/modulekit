5599dc6542c1c25f6c98ae95db57561bf29b2a6a
zeroknots <zeroknots@protonmail.com>
2024-04-24 13:51:32 +0700

    ERC1271 working

--- examples/test/MultiFactor/MultiFactor.t.sol
+++ examples/test/MultiFactor/MultiFactor.t.sol
@@ -5,6 +5,7 @@ import "forge-std/Test.sol";
 import "modulekit/src/ModuleKit.sol";
 import "modulekit/src/Modules.sol";
 import "modulekit/src/Mocks.sol";
+import "modulekit/src/interfaces/IERC1271.sol";
 
 import { MultiFactor } from "src/MultiFactor/MultiFactor.sol";
 import { MultiFactorLib } from "src/MultiFactor/MultiFactorLib.sol";
@@ -117,5 +118,24 @@ contract MultiFactorTest is RhinestoneModuleKit, Test {
         userOpData.execUserOps();
 
         assertEq(token.balanceOf(recipient), 10 ether);
+    }
+
+    function test_ERC1271() public {
+        Validator[] memory validators = new Validator[](2);
+        validators[0] = Validator({
+            packedValidatorAndId: bytes32(abi.encodePacked(uint96(1), address(validator1))),
+            data: hex"41414141"
+        });
+
+        validators[1] = Validator({
+            packedValidatorAndId: bytes32(abi.encodePacked(uint96(1), address(validator2))),
+            data: hex"41414141"
+        });
+
+        bytes4 magicValue = IERC1271(instance.account).isValidSignature(
+            bytes32(hex"4141"), abi.encodePacked(address(mfa), abi.encode(validators))
+        );
+
+        assertEq(magicValue, IERC1271.isValidSignature.selector);
     }
 }
