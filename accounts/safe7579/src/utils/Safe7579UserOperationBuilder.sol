7ac113c9a5b758fab5993ea800d1170cc6c4c14d
kopy-kat <kopp.konrad@gmail.com>
2024-05-01 22:32:31 +0100

    feat: update userop builder to latest interface

--- accounts/safe7579/src/utils/Safe7579UserOperationBuilder.sol
+++ accounts/safe7579/src/utils/Safe7579UserOperationBuilder.sol
@@ -2,47 +2,51 @@
 pragma solidity ^0.8.24;
 
 import {
-    IUserOpConstructor, PackedUserOperation
-} from "src/interfaces/IUserOperationConstructor.sol";
+    IUserOperationBuilder, PackedUserOperation
+} from "src/interfaces/IUserOperationBuilder.sol";
 import { ModeLib } from "erc7579/lib/ModeLib.sol";
 import { Execution, ExecutionLib } from "erc7579/lib/ExecutionLib.sol";
 import { IEntryPoint } from "@ERC4337/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 import { IERC7579Account } from "erc7579/interfaces/IERC7579Account.sol";
 
-contract Safe7579UserOpConstructor is IUserOpConstructor {
-    IEntryPoint public immutable entryPoint;
+contract Safe7579UserOperationBuilder is IUserOperationBuilder {
+    IEntryPoint internal immutable _entryPoint;
 
-    constructor(address _entryPoint) {
-        entryPoint = IEntryPoint(_entryPoint);
+    constructor(address _entryPointAddress) {
+        _entryPoint = IEntryPoint(_entryPointAddress);
     }
 
-    function getNonceWithContext(
+    function entryPoint() external view returns (address) {
+        return address(_entryPoint);
+    }
+
+    function getNonce(
         address smartAccount,
-        bytes calldata permissionsContext
+        bytes calldata context
     )
         external
         view
         returns (uint256)
     {
-        address validator = address(bytes20(permissionsContext[0:20]));
+        address validator = address(bytes20(context[0:20]));
         uint192 key = uint192(bytes24(bytes20(address(validator))));
-        return entryPoint.getNonce(address(smartAccount), key);
+        return _entryPoint.getNonce(address(smartAccount), key);
     }
 
-    function getCallDataWithContext(
+    function getCallData(
         address smartAccount,
         Execution[] calldata executions,
-        bytes calldata permissionsContext
+        bytes calldata context
     )
         external
         view
-        returns (bytes memory callDataWithContext)
+        returns (bytes memory)
     {
         if (executions.length == 0) {
             revert("No executions provided");
         }
         if (executions.length == 1) {
-            callDataWithContext = abi.encodeCall(
+            return abi.encodeCall(
                 IERC7579Account.execute,
                 (
                     ModeLib.encodeSimpleSingle(),
@@ -52,7 +56,7 @@ contract Safe7579UserOpConstructor is IUserOpConstructor {
                 )
             );
         } else {
-            callDataWithContext = abi.encodeCall(
+            return abi.encodeCall(
                 IERC7579Account.execute,
                 (ModeLib.encodeSimpleBatch(), ExecutionLib.encodeBatch(executions))
             );
@@ -60,14 +64,27 @@ contract Safe7579UserOpConstructor is IUserOpConstructor {
         // TODO: add delegatecall, tryExecute and other execution modes handling
     }
 
-    function getSignatureWithContext(
+    function getDummySignature(
         address smartAccount,
-        PackedUserOperation calldata userOp,
-        bytes calldata permissionsContext
+        Execution[] calldata executions,
+        bytes calldata context
     )
         external
+        view
         returns (bytes memory signature)
     {
-        signature = userOp.signature;
+        return context;
+    }
+
+    function getSignature(
+        address smartAccount,
+        PackedUserOperation calldata userOperation,
+        bytes calldata context
+    )
+        external
+        view
+        returns (bytes memory signature)
+    {
+        signature = userOperation.signature;
     }
 }
