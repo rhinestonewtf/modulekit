cc6a8f4ec8c022473c776e8842b1d8fdf1846661
kopy-kat <kopp.konrad@gmail.com>
2024-05-06 10:35:58 +0100

    chore: deployments

--- examples/script/Deploy.s.sol
+++ examples/script/Deploy.s.sol
@@ -1,48 +1,47 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.19;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-// import { Script } from "forge-std/Script.sol";
-// import { AutoSavings } from "../src/AutoSavings/AutoSavings.sol";
-// import { FlashloanCallback } from "../src/ColdStorage/FlashloanCallback.sol";
-// import { FlashloanLender } from "../src/ColdStorage/FlashloanLender.sol";
-// import { ColdStorageHook } from "../src/ColdStorage/ColdStorageHook.sol";
-// import { ColdStorageExecutor } from "../src/ColdStorage/ColdStorageExecutor.sol";
-// import { DeadmanSwitch } from "../src/DeadmanSwitch/DeadmanSwitch.sol";
-// import { MultiFactor } from "../src/MultiFactor/MultiFactor.sol";
-// import { OwnableValidator } from "../src/OwnableValidator/OwnableValidator.sol";
-// import { ScheduledOrders } from "../src/ScheduledOrders/ScheduledOrders.sol";
-// import { ScheduledTransfers } from "../src/ScheduledTransfers/ScheduledTransfers.sol";
-// import { WebAuthnValidator } from "../src/WebAuthnValidator/WebAuthnValidator.sol";
+import { Script } from "forge-std/Script.sol";
+import { IERC7484 } from "modulekit/src/interfaces/IERC7484.sol";
 
-// /**
-//  * @title Deploy
-//  * @author @kopy-kat
-//  */
-// contract DeployScript is Script {
-//     function run() public {
-//         bytes32 salt = bytes32(uint256(0));
+import { AutoSavings } from "src/AutoSavings/AutoSavings.sol";
+import { ColdStorageHook } from "src/ColdStorageHook/ColdStorageHook.sol";
+import { ColdStorageFlashloan } from "src/ColdStorageHook/ColdStorageFlashloan.sol";
+import { DeadmanSwitch } from "src/DeadmanSwitch/DeadmanSwitch.sol";
+import { HookMultiPlexer } from "src/HookMultiPlexer/HookMultiPlexer.sol";
+import { MultiFactor } from "src/MultiFactor/MultiFactor.sol";
+import { OwnableExecutor } from "src/OwnableExecutor/OwnableExecutor.sol";
+import { OwnableValidator } from "src/OwnableValidator/OwnableValidator.sol";
+import { RegistryHook } from "src/RegistryHook/RegistryHook.sol";
+import { ScheduledOrders } from "src/ScheduledOrders/ScheduledOrders.sol";
+import { ScheduledTransfers } from "src/ScheduledTransfers/ScheduledTransfers.sol";
+import { SocialRecovery } from "src/SocialRecovery/SocialRecovery.sol";
 
-//         vm.startBroadcast(vm.envUint("PK"));
+/**
+ * @title Deploy
+ * @author @kopy-kat
+ */
+contract DeployScript is Script {
+    function run() public {
+        bytes32 salt = bytes32(uint256(0));
+        IERC7484 registry = IERC7484(0x1D8c40F958Fb6998067e9B8B26850d2ae30b7c70); // mock registry
 
-//         // Deploy Modules
-//         AutoSavings autoSavings = new AutoSavings{ salt: salt }();
+        vm.startBroadcast(vm.envUint("PK"));
 
-//         FlashloanCallback flashloanCallback = new FlashloanCallback{ salt: salt }();
-//         FlashloanLender flashloanLender = new FlashloanLender{ salt: salt }();
-//         ColdStorageHook coldStorageHook = new ColdStorageHook{ salt: salt }();
-//         ColdStorageExecutor coldStorageExecutor = new ColdStorageExecutor{ salt: salt }();
+        // Deploy Modules
+        new AutoSavings{ salt: salt }();
+        new ColdStorageHook{ salt: salt }();
+        new ColdStorageFlashloan{ salt: salt }();
+        new DeadmanSwitch{ salt: salt }();
+        new HookMultiPlexer{ salt: salt }(registry);
+        new MultiFactor{ salt: salt }(registry);
+        new OwnableExecutor{ salt: salt }();
+        new OwnableValidator{ salt: salt }();
+        new RegistryHook{ salt: salt }();
+        new ScheduledOrders{ salt: salt }();
+        new ScheduledTransfers{ salt: salt }();
+        new SocialRecovery{ salt: salt }();
 
-//         DeadmanSwitch deadmanSwitch = new DeadmanSwitch{ salt: salt }();
-
-//         MultiFactor multiFactor = new MultiFactor{ salt: salt }();
-
-//         OwnableValidator ownableValidator = new OwnableValidator{ salt: salt }();
-
-//         ScheduledOrders scheduledOrders = new ScheduledOrders{ salt: salt }();
-//         ScheduledTransfers scheduledTransfers = new ScheduledTransfers{ salt: salt }();
-
-//         WebAuthnValidator webAuthnValidator = new WebAuthnValidator{ salt: salt }();
-
-//         vm.stopBroadcast();
-//     }
-// }
+        vm.stopBroadcast();
+    }
+}
