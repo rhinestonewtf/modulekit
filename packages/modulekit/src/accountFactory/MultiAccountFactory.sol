79adab253914b5daf61d5c18d7aa00ce9feb6162
faheelsattar <faheelsattar@yahoo.com>
2024-05-13 12:04:12 +0500

    feat: add kernel factory

--- packages/modulekit/src/accountFactory/MultiAccountFactory.sol
+++ packages/modulekit/src/accountFactory/MultiAccountFactory.sol
@@ -4,24 +4,40 @@ pragma solidity ^0.8.23;
 import "forge-std/Base.sol";
 import "./safe7579/Safe7579Factory.sol";
 import "./referenceImpl/RefImplFactory.sol";
-import { ERC7579BootstrapConfig } from "../external/ERC7579.sol";
+import {ERC7579BootstrapConfig} from "../external/ERC7579.sol";
+import {Kernel7579Factory} from "./kernel7579/Kernel7579Factory.sol";
 
 enum AccountType {
     DEFAULT,
-    SAFE7579
+    SAFE7579,
+    KERNEL7579
 }
 
 string constant DEFAULT = "DEFAULT";
 string constant SAFE7579 = "SAFE7579";
+string constant KERNEL7579 = "KERNEL7579";
 
-contract MultiAccountFactory is TestBase, Safe7579Factory, RefImplFactory {
+contract MultiAccountFactory is
+    TestBase,
+    Safe7579Factory,
+    RefImplFactory,
+    Kernel7579Factory
+{
     AccountType public env;
 
     constructor() {
         string memory _env = vm.envOr("ACCOUNT_TYPE", DEFAULT);
 
-        if (keccak256(abi.encodePacked(_env)) == keccak256(abi.encodePacked(SAFE7579))) {
+        if (
+            keccak256(abi.encodePacked(_env)) ==
+            keccak256(abi.encodePacked(SAFE7579))
+        ) {
             env = AccountType.SAFE7579;
+        } else if (
+            keccak256(abi.encodePacked(_env)) ==
+            keccak256(abi.encodePacked(KERNEL7579))
+        ) {
+            env = AccountType.KERNEL7579;
         } else {
             env = AccountType.DEFAULT;
         }
@@ -30,36 +46,46 @@ contract MultiAccountFactory is TestBase, Safe7579Factory, RefImplFactory {
     function createAccount(
         bytes32 salt,
         bytes calldata initCode
-    )
-        public
-        returns (address account)
-    {
+    ) public returns (address account) {
         if (env == AccountType.SAFE7579) {
             return _makeSafe(salt, initCode);
+        } else if (env == AccountType.KERNEL7579) {
+            //TODO check for data length
+            return _makeKernel(initCode, salt);
         } else {
             return _makeDefault(salt, initCode);
         }
     }
 
-    function _makeDefault(bytes32 salt, bytes calldata initCode) public returns (address) {
+    function _makeDefault(
+        bytes32 salt,
+        bytes calldata initCode
+    ) public returns (address) {
         return _createUMSA(salt, initCode);
     }
 
-    function _makeSafe(bytes32 salt, bytes calldata initCode) public returns (address) {
+    function _makeSafe(
+        bytes32 salt,
+        bytes calldata initCode
+    ) public returns (address) {
         return _createSafe(salt, initCode);
+    }
+
+    function _makeKernel(
+        bytes calldata data,
+        bytes32 salt
+    ) public returns (address) {
+        return _createKernel(data, salt);
     }
 
     function getAddress(
         bytes32 salt,
         bytes memory initCode
-    )
-        public
-        view
-        virtual
-        returns (address)
-    {
+    ) public view virtual returns (address) {
         if (env == AccountType.SAFE7579) {
             return getAddressSafe(salt, initCode);
+        } else if (env == AccountType.KERNEL7579) {
+            return getAddressKernel(initCode, salt);
         } else {
             return getAddressUMSA(salt, initCode);
         }
@@ -83,22 +109,30 @@ contract MultiAccountFactory is TestBase, Safe7579Factory, RefImplFactory {
         ERC7579BootstrapConfig[] calldata _executors,
         ERC7579BootstrapConfig calldata _hook,
         ERC7579BootstrapConfig[] calldata _fallbacks
-    )
-        external
-        view
-        returns (bytes memory init)
-    {
+    ) external view returns (bytes memory init) {
         if (env == AccountType.SAFE7579) {
             init = abi.encode(
                 address(bootstrapSafe),
                 abi.encodeCall(
-                    ERC7579Bootstrap.initMSA, (_validators, _executors, _hook, _fallbacks)
+                    ERC7579Bootstrap.initMSA,
+                    (_validators, _executors, _hook, _fallbacks)
+                )
+            );
+        } else if (env == AccountType.KERNEL7579) {
+            init = abi.encode(
+                address(bootstrapDefault),
+                abi.encodeCall(
+                    ERC7579Bootstrap.initMSA,
+                    (_validators, _executors, _hook, _fallbacks)
                 )
             );
         } else {
             init = abi.encode(
                 address(bootstrapDefault),
-                abi.encodeCall(BootstrapSafe.initMSA, (_validators, _executors, _hook, _fallbacks))
+                abi.encodeCall(
+                    BootstrapSafe.initMSA,
+                    (_validators, _executors, _hook, _fallbacks)
+                )
             );
         }
     }
