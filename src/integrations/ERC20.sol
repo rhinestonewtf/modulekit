02365a17173bf758cca265f4d525e8eb5dda48c0
zeroknots <zeroknots@protonmail.com>
2024-06-19 10:13:28 +0700

    hotfix: add safeERC20 to integrations
    wip
    
    feat: add safe
    
    wip

--- src/integrations/ERC20.sol
+++ src/integrations/ERC20.sol
@@ -3,8 +3,34 @@ pragma solidity ^0.8.23;
 
 import { IERC20 } from "forge-std/interfaces/IERC20.sol";
 import { Execution } from "../Accounts.sol";
+import { ERC7579Exec } from "./ERC7579Exec.sol";
 
 library ERC20Integration {
+    using ERC7579Exec for address;
+
+    error SafeERC20TransferFailed();
+
+    function safeApprove(
+        IERC20 token,
+        address spender,
+        uint256 amount
+    )
+        internal
+        pure
+        returns (Execution memory exec0, Execution memory exec1)
+    {
+        exec0 = Execution({
+            target: address(token),
+            value: 0,
+            callData: abi.encodeCall(IERC20.approve, (spender, 0))
+        });
+        exec1 = Execution({
+            target: address(token),
+            value: 0,
+            callData: abi.encodeCall(IERC20.approve, (spender, amount))
+        });
+    }
+
     function approve(
         IERC20 token,
         address spender,
@@ -19,6 +45,49 @@ library ERC20Integration {
             value: 0,
             callData: abi.encodeCall(IERC20.approve, (spender, amount))
         });
+    }
+
+    function safeTransfer(IERC20 token, address to, uint256 amount) internal {
+        safeTransfer(token, msg.sender, to, amount);
+    }
+
+    function safeTransfer(IERC20 token, address account, address to, uint256 amount) internal {
+        bytes memory ret = account.exec7579({
+            to: address(token),
+            value: 0,
+            data: abi.encodeCall(IERC20.transfer, (to, amount))
+        });
+        bytes[] memory retValues = abi.decode(ret, (bytes[]));
+        if (retValues[0].length != 0) {
+            bool success = abi.decode(retValues[0], (bool));
+            if (!success) revert SafeERC20TransferFailed();
+        }
+    }
+
+    function safeTransferFrom(IERC20 token, address from, address to, uint256 amount) internal {
+        safeTransferFrom(token, msg.sender, from, to, amount);
+    }
+
+    function safeTransferFrom(
+        IERC20 token,
+        address account,
+        address from,
+        address to,
+        uint256 amount
+    )
+        internal
+    {
+        bytes memory ret = account.exec7579({
+            to: address(token),
+            value: 0,
+            data: abi.encodeCall(IERC20.transferFrom, (from, to, amount))
+        });
+
+        bytes[] memory retValues = abi.decode(ret, (bytes[]));
+        if (retValues[0].length != 0) {
+            bool success = abi.decode(retValues[0], (bool));
+            if (!success) revert SafeERC20TransferFailed();
+        }
     }
 
     function transfer(
---
+++ src/integrations/ERC7579Exec.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.25;
+
+import "../external/ERC7579.sol";
+
+library ERC7579Exec {
+    function exec7579(
+        address account,
+        address to,
+        uint256 value,
+        bytes memory data
+    )
+        internal
+        returns (bytes memory result)
+    {
+        ModeCode modeCode = ERC7579ModeLib.encode({
+            callType: CALLTYPE_SINGLE,
+            execType: EXECTYPE_DEFAULT,
+            mode: MODE_DEFAULT,
+            payload: ModePayload.wrap(bytes22(0))
+        });
+
+        return IERC7579Account(account).executeFromExecutor(
+            modeCode, ERC7579ExecutionLib.encodeSingle(to, value, data)
+        )[0];
+    }
+
+    function exec7579(
+        address to,
+        uint256 value,
+        bytes memory data
+    )
+        internal
+        returns (bytes memory result)
+    {
+        return exec7579(msg.sender, to, value, data);
+    }
+
+    function exec7579(
+        address account,
+        Execution[] memory execs
+    )
+        internal
+        returns (bytes[] memory results)
+    {
+        ModeCode modeCode = ERC7579ModeLib.encode({
+            callType: CALLTYPE_BATCH,
+            execType: EXECTYPE_DEFAULT,
+            mode: MODE_DEFAULT,
+            payload: ModePayload.wrap(bytes22(0))
+        });
+        results = IERC7579Account(account).executeFromExecutor(
+            modeCode, ERC7579ExecutionLib.encodeBatch(execs)
+        );
+    }
+
+    function exec7579(Execution[] memory execs) internal returns (bytes[] memory results) {
+        return exec7579(msg.sender, execs);
+    }
+
+    // Note: Not every account will support delegatecalls
+    function exec7579(
+        address account,
+        address delegateTarget,
+        bytes memory callData
+    )
+        internal
+        returns (bytes[] memory results)
+    {
+        ModeCode modeCode = ERC7579ModeLib.encode({
+            callType: CALLTYPE_DELEGATECALL,
+            execType: EXECTYPE_DEFAULT,
+            mode: MODE_DEFAULT,
+            payload: ModePayload.wrap(bytes22(0))
+        });
+        results = IERC7579Account(account).executeFromExecutor(
+            modeCode, abi.encodePacked(delegateTarget, callData)
+        );
+    }
+
+    // Note: Not every account will support delegatecalls
+    function exec7579(
+        address delegateTarget,
+        bytes memory callData
+    )
+        internal
+        returns (bytes[] memory results)
+    {
+        return exec7579(msg.sender, delegateTarget, callData);
+    }
+}
