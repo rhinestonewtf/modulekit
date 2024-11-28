48d40287cd3cf18d0c5a8e82c8d096f204897732
highskore <luka@racki.dev>
2024-11-28 19:25:12 +0100

    fix: import ExecutionLib

--- src/Accounts.sol
+++ src/Accounts.sol
@@ -2,5 +2,5 @@
 pragma solidity >=0.8.23 <0.9.0;
 
 /* solhint-disable no-unused-import */
-import { Execution } from "src/accounts/erc7579/lib/ExecutionLib.sol";
+import { Execution, ExecutionLib } from "src/accounts/erc7579/lib/ExecutionLib.sol";
 import { IERC7579Account } from "src/accounts/common/interfaces/IERC7579Account.sol";
